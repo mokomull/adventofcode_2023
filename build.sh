354be61dfc7ffe847f0b2691a777c8d6765ab5a8
Matt Mullins <mmullins@mmlx.us>
2023-12-04 23:51:35 -0800

    build.sh: generate paths for my hydra:public_html
    Apparently Jekyll has some long-standing angst against generating
    relative paths.  Passing in `-b` at least makes the resulting HTML look
    like last year's...

--- build.sh
+++ build.sh
@@ -4,4 +4,11 @@ PATH="/usr/local/opt/ruby@3.1/bin:$PATH"
     cd omnibus
     [[ $1 == "debug" ]] && wasm-pack build -t web --debug || wasm-pack build -t web --release
 )
-(cd site && bundle exec jekyll build)
+(
+    cd site
+    if [[ $1 == "publish" ]] ; then
+        bundle exec jekyll build -b /~mmullins/adventofcode_2023
+    else
+        bundle exec jekyll build
+    fi
+)
