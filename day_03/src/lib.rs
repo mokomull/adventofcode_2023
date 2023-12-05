064dde145fdca807a7296ab9059b9c2ef0b8df69
Matt Mullins <mmullins@mmlx.us>
2023-12-04 16:54:24 -0800

    prelude, day_03: factor-out iterating nearby cells
    Advent of Code does a lot of problems involving "if [something] in an
    adjacent space", so go ahead and make that into a helper function.

--- day_03/src/lib.rs
+++ day_03/src/lib.rs
@@ -1,5 +1,3 @@
-use std::cmp::min;
-
 use prelude::*;
 
 pub struct Solution(Vec<Vec<u8>>);
@@ -25,40 +23,38 @@ impl Solution {
                 log::debug!("symbol at {:?}: {:?}", (row, col), c);
 
                 // we're on a symbol, so check all around it for a digit
-                for x in row.saturating_sub(1)..=min(row + 1, self.0.len() - 1) {
-                    for y in col.saturating_sub(1)..=min(col + 1, self.0[0].len() - 1) {
-                        if (x, y) == (row, col) || visited.contains(&(x, y)) {
-                            continue;
-                        }
+                for (x, y) in adjacent_including_diagonal(&self.0, row, col) {
+                    if (x, y) == (row, col) || visited.contains(&(x, y)) {
+                        continue;
+                    }
 
-                        if self.0[x][y].is_ascii_digit() {
-                            // part numbers only exist within a single row, so just look left and right for more digits
-                            let string = &*self.0[x];
-                            let first = (0..y)
-                                .rev()
-                                .find(|&i| !string[i].is_ascii_digit())
-                                .map(
-                                    // add one, because we found the first position left of (x, y) that is *not* a digit
-                                    |i| i + 1,
-                                )
-                                .unwrap_or(0);
-                            let last = (y..string.len())
-                                .find(|&i| !string[i].is_ascii_digit())
-                                .unwrap_or(string.len());
-                            log::debug!("{first} {last}");
+                    if self.0[x][y].is_ascii_digit() {
+                        // part numbers only exist within a single row, so just look left and right for more digits
+                        let string = &*self.0[x];
+                        let first = (0..y)
+                            .rev()
+                            .find(|&i| !string[i].is_ascii_digit())
+                            .map(
+                                // add one, because we found the first position left of (x, y) that is *not* a digit
+                                |i| i + 1,
+                            )
+                            .unwrap_or(0);
+                        let last = (y..string.len())
+                            .find(|&i| !string[i].is_ascii_digit())
+                            .unwrap_or(string.len());
+                        log::debug!("{first} {last}");
 
-                            let part_number = std::str::from_utf8(&string[first..last])
-                                .context("digits should be UTF-8")?;
-                            log::debug!("found part number: {:?}", part_number);
+                        let part_number = std::str::from_utf8(&string[first..last])
+                            .context("digits should be UTF-8")?;
+                        log::debug!("found part number: {:?}", part_number);
 
-                            sum += part_number
-                                .parse::<u64>()
-                                .context("digits should also parse into a u64")?;
+                        sum += part_number
+                            .parse::<u64>()
+                            .context("digits should also parse into a u64")?;
 
-                            // and mark that part number as "used" so we don't double-count it
-                            for i in first..last {
-                                visited.insert((x, i));
-                            }
+                        // and mark that part number as "used" so we don't double-count it
+                        for i in first..last {
+                            visited.insert((x, i));
                         }
                     }
                 }
@@ -84,42 +80,40 @@ impl Solution {
                 log::debug!("symbol at {:?}: {:?}", (row, col), c);
 
                 // we're on a symbol, so check all around it for a digit
-                for x in row.saturating_sub(1)..=min(row + 1, self.0.len() - 1) {
-                    for y in col.saturating_sub(1)..=min(col + 1, self.0[0].len() - 1) {
-                        if (x, y) == (row, col) || visited.contains(&(x, y)) {
-                            continue;
-                        }
+                for (x, y) in adjacent_including_diagonal(&self.0, row, col) {
+                    if (x, y) == (row, col) || visited.contains(&(x, y)) {
+                        continue;
+                    }
 
-                        if self.0[x][y].is_ascii_digit() {
-                            // part numbers only exist within a single row, so just look left and right for more digits
-                            let string = &*self.0[x];
-                            let first = (0..y)
-                                .rev()
-                                .find(|&i| !string[i].is_ascii_digit())
-                                .map(
-                                    // add one, because we found the first position left of (x, y) that is *not* a digit
-                                    |i| i + 1,
-                                )
-                                .unwrap_or(0);
-                            let last = (y..string.len())
-                                .find(|&i| !string[i].is_ascii_digit())
-                                .unwrap_or(string.len());
-                            log::debug!("{first} {last}");
+                    if self.0[x][y].is_ascii_digit() {
+                        // part numbers only exist within a single row, so just look left and right for more digits
+                        let string = &*self.0[x];
+                        let first = (0..y)
+                            .rev()
+                            .find(|&i| !string[i].is_ascii_digit())
+                            .map(
+                                // add one, because we found the first position left of (x, y) that is *not* a digit
+                                |i| i + 1,
+                            )
+                            .unwrap_or(0);
+                        let last = (y..string.len())
+                            .find(|&i| !string[i].is_ascii_digit())
+                            .unwrap_or(string.len());
+                        log::debug!("{first} {last}");
 
-                            let part_number = std::str::from_utf8(&string[first..last])
-                                .context("digits should be UTF-8")?;
-                            log::debug!("found part number: {:?}", part_number);
+                        let part_number = std::str::from_utf8(&string[first..last])
+                            .context("digits should be UTF-8")?;
+                        log::debug!("found part number: {:?}", part_number);
 
-                            part_numbers.push(
-                                part_number
-                                    .parse::<u64>()
-                                    .context("digits should also parse into a u64")?,
-                            );
+                        part_numbers.push(
+                            part_number
+                                .parse::<u64>()
+                                .context("digits should also parse into a u64")?,
+                        );
 
-                            // and mark that part number as "used" so we don't double-count it
-                            for i in first..last {
-                                visited.insert((x, i));
-                            }
+                        // and mark that part number as "used" so we don't double-count it
+                        for i in first..last {
+                            visited.insert((x, i));
                         }
                     }
                 }
--- prelude/src/lib.rs
+++ prelude/src/lib.rs
@@ -1,4 +1,28 @@
+use std::cmp::min;
+
 pub use prelude_2022::*;
 
 pub extern crate anyhow;
 pub use anyhow::Context;
+
+/// Iterate through the (up to) eight locations that are adjacent to (x, y),
+/// where `data` is accessed via `data[x][y]`.
+///
+/// # Panics
+///
+/// Panics if `data` is empty or non-square.
+pub fn adjacent_including_diagonal<T>(
+    data: &Vec<Vec<T>>,
+    x: usize,
+    y: usize,
+) -> impl Iterator<Item = (usize, usize)> {
+    let first_index = x.saturating_sub(1)..=min(x + 1, data.len() - 1);
+    // just use data[0], which assumes that the input is
+    // (a) non-empty, and
+    // (b) square.
+    let second_index = y.saturating_sub(1)..=min(y + 1, data[0].len() - 1);
+
+    first_index
+        .cartesian_product(second_index)
+        .filter(move |&(i, j)| (x, y) != (i, j))
+}
