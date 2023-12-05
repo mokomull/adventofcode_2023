02d284c2f9e2f403ca7cbaf82bfdbcbdbedb59ac
Matt Mullins <mmullins@mmlx.us>
2023-12-04 21:24:13 -0800

    day 05: start on a parser
    I don't immediately see why these iterators' lifetimes are
    overlapping...
    
    	error[E0499]: cannot borrow `lines` as mutable more than once at a time
    	  --> day_05/src/lib.rs:60:20
    	   |
    	57 |             let triples = get_triples(&mut lines);
    	   |                                       ---------- first mutable borrow occurs here
    	...
    	60 |         assert_eq!(lines.next().unwrap(), "soil-to-fertilizer map:");
    	   |                    ^^^^^
    	   |                    |
    	   |                    second mutable borrow occurs here
    	   |                    first borrow later used here
    
    	error[E0499]: cannot borrow `lines` as mutable more than once at a time
    	  --> day_05/src/lib.rs:61:56
    	   |
    	57 |             let triples = get_triples(&mut lines);
    	   |                                       ---------- first mutable borrow occurs here
    	...
    	61 |         let soil_to_fertilizer = parse_map(get_triples(&mut lines));
    	   |                                                        ^^^^^^^^^^
    	   |                                                        |
    	   |                                                        second mutable borrow occurs here
    	   |                                                        first borrow later used here

--- day_05/src/lib.rs
+++ day_05/src/lib.rs
@@ -1,10 +1,85 @@
 use prelude::*;
 
-pub struct Solution();
+type Map = HashMap<u64, u64>;
+
+pub struct Solution {
+    seeds: Vec<u64>,
+    seed_to_soil: Map,
+    soil_to_fertilizer: Map,
+    fertilizer_to_water: Map,
+    water_to_light: Map,
+    light_to_temperature: Map,
+    temperature_to_humidity: Map,
+    humidity_to_location: Map,
+}
+
+fn get_triples<'a>(
+    it: &'a mut impl Iterator<Item = &'a str>,
+) -> impl Iterator<Item = (u64, u64, u64)> + 'a {
+    it.take_while(|x| !x.is_empty())
+        .map(|line| {
+            line.split_whitespace()
+                .map(|x| x.parse().expect("not an integer"))
+                .collect_vec()
+                .try_into()
+                .expect("more or fewer than three in a line")
+        })
+        .map(|junk: [u64; 3]| (junk[0], junk[1], junk[2]))
+}
+
+fn parse_map(it: impl Iterator<Item = (u64, u64, u64)>) -> Map {
+    let mut map = Map::new();
+
+    for (from, to, count) in it {
+        for offset in 0..count {
+            map.insert(from + offset, to + offset);
+        }
+    }
+
+    map
+}
 
 impl Solution {
     pub fn new(input: &str) -> Solution {
-        Solution()
+        init();
+
+        let mut lines = input.lines();
+
+        let seeds = lines.next().expect("no seeds");
+        let seeds = seeds
+            .split_whitespace()
+            .map(|seed| seed.parse().expect("seed not an integer?"))
+            .collect();
+        assert_eq!(lines.next().unwrap(), "");
+
+        assert_eq!(lines.next().unwrap(), "seed-to-soil map:");
+        let seed_to_soil = {
+            let triples = get_triples(&mut lines);
+            parse_map(triples)
+        };
+        assert_eq!(lines.next().unwrap(), "soil-to-fertilizer map:");
+        let soil_to_fertilizer = parse_map(get_triples(&mut lines));
+        assert_eq!(lines.next().unwrap(), "fertilizer-to-water map:");
+        let fertilizer_to_water = parse_map(get_triples(&mut lines));
+        assert_eq!(lines.next().unwrap(), "water-to-light map:");
+        let water_to_light = parse_map(get_triples(&mut lines));
+        assert_eq!(lines.next().unwrap(), "light-to-temperature map:");
+        let light_to_temperature = parse_map(get_triples(&mut lines));
+        assert_eq!(lines.next().unwrap(), "temperature-to-humidity map:");
+        let temperature_to_humidity = parse_map(get_triples(&mut lines));
+        assert_eq!(lines.next().unwrap(), "humidity-to-location map:");
+        let humidity_to_location = parse_map(get_triples(&mut lines));
+
+        Solution {
+            seeds,
+            seed_to_soil,
+            soil_to_fertilizer,
+            fertilizer_to_water,
+            water_to_light,
+            light_to_temperature,
+            temperature_to_humidity,
+            humidity_to_location,
+        }
     }
 
     pub fn part1(&self) -> anyhow::Result<u64> {
