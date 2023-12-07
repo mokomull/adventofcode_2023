718a005d087116094e68db1465b75d4cbec06d17
Matt Mullins <mmullins@mmlx.us>
2023-12-06 17:40:34 -0800

    day 06: parser?

--- day_06/src/lib.rs
+++ day_06/src/lib.rs
@@ -1,10 +1,45 @@
 use prelude::*;
 
-pub struct Solution();
+struct Race {
+    time: u64,
+    record: u64,
+}
+
+pub struct Solution(Vec<Race>);
 
 impl Solution {
     pub fn new(input: &str) -> Solution {
-        Solution()
+        let mut lines = input.lines();
+        let times = lines
+            .next()
+            .unwrap()
+            .split_once(':')
+            .unwrap()
+            .1
+            .trim_start()
+            .split_whitespace()
+            .map(str::parse)
+            .collect::<Result<Vec<u64>, _>>()
+            .unwrap();
+        let records = lines
+            .next()
+            .unwrap()
+            .split_once(':')
+            .unwrap()
+            .1
+            .trim_start()
+            .split_whitespace()
+            .map(str::parse)
+            .collect::<Result<Vec<u64>, _>>()
+            .unwrap();
+
+        Solution(
+            times
+                .into_iter()
+                .zip(records)
+                .map(|(time, record)| Race { time, record })
+                .collect(),
+        )
     }
 
     pub fn part1(&self) -> anyhow::Result<u64> {
