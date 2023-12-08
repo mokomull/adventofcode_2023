cbea1c238a5115e7d49a42604507839e7fb247f2
Matt Mullins <mmullins@mmlx.us>
2023-12-07 22:25:10 -0800

    day 08: check *all* possible steps_to_end

--- day_08/src/lib.rs
+++ day_08/src/lib.rs
@@ -62,20 +62,20 @@ impl Solution {
         struct Data {
             steps_to_cycle_start: usize,
             cycle_length: usize,
-            steps_to_end: usize,
+            steps_to_end: Vec<usize>,
         }
         let mut data = HashMap::new();
 
         for s in starting {
             log::debug!("working on {s:?}");
-            let mut steps_to_end = None;
+            let mut steps_to_end = vec![];
 
             let mut current = s;
             let mut seen = HashMap::new();
             for (i, direction) in self.directions.bytes().cycle().enumerate() {
                 log::debug!("visiting {current:?}");
-                if steps_to_end.is_none() && current.ends_with('Z') {
-                    steps_to_end = Some(i);
+                if current.ends_with('Z') {
+                    steps_to_end.push(i);
                 }
 
                 // because not only do we have to be at the same node, but we
@@ -89,7 +89,7 @@ impl Solution {
                         Data {
                             steps_to_cycle_start,
                             cycle_length: i - steps_to_cycle_start,
-                            steps_to_end: steps_to_end.unwrap(),
+                            steps_to_end,
                         },
                     );
                     break;
@@ -107,21 +107,23 @@ impl Solution {
         log::info!("collected data: {:?}", data);
 
         let res;
-        let longest_cycle = data.values().max_by_key(|d| d.cycle_length).unwrap();
+        // assuming that there's one cycle that only hits the end state once,
+        // let's use that as our step size for simplicity.
+        let only_once_in_cycle = data.values().find(|d| d.steps_to_end.len() == 1).unwrap();
 
-        let mut i = longest_cycle.steps_to_end as u64;
+        let mut i = only_once_in_cycle.steps_to_end[0] as u64;
         loop {
             log::debug!("looking at {}", i);
-            if data
-                .values()
-                .all(|d| i % d.cycle_length as u64 == d.steps_to_end as u64 % d.cycle_length as u64)
-            {
+            if data.values().all(|d| {
+                d.steps_to_end.iter().any(|&steps_to_end| {
+                    i % d.cycle_length as u64 == steps_to_end as u64 % d.cycle_length as u64
+                })
+            }) {
                 res = i;
                 break;
             }
 
-            // make the largest steps we can.
-            i += longest_cycle.cycle_length as u64;
+            i += only_once_in_cycle.cycle_length as u64;
         }
 
         assert!(data.values().all(|d| res > d.steps_to_cycle_start as u64));
