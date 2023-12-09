378c984ccc0571d2d773c4a989f73cd0a422faa7
Matt Mullins <mmullins@mmlx.us>
2023-12-08 21:02:56 -0800

    day 09: parser

--- day_09/src/lib.rs
+++ day_09/src/lib.rs
@@ -1,10 +1,14 @@
 use prelude::*;
 
-pub struct Solution();
+pub struct Solution(Vec<Vec<u64>>);
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
-        Solution()
+        Solution(
+            input.lines().map(|line| {
+                line.split_whitespace().map(|num| num.parse().expect("can't parse input number")).collect()
+            }).collect()
+        )
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
