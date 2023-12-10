1350ef0e2b2067646c62ba864b3c8b6b1a774a63
Matt Mullins <mmullins@mmlx.us>
2023-12-09 22:20:14 -0800

    day 10: split web/omnibus logic out
    This is in preparation for a visualization so I can see what I did
    wrong.

---
+++ omnibus/src/day10.rs
@@ -0,0 +1,3 @@
+use wasm_bindgen::prelude::*;
+
+crate::common_day!(day_10, Day10, u64, u64);
--- omnibus/src/lib.rs
+++ omnibus/src/lib.rs
@@ -1,5 +1,7 @@
 use wasm_bindgen::prelude::*;
 
+mod day10;
+
 macro_rules! common_day {
     ($krate: ident, $strukt: ident, $part1_result: ty, $part2_result: ty) => {
         #[wasm_bindgen]
@@ -22,6 +24,8 @@ macro_rules! common_day {
     };
 }
 
+use common_day; // let submodule use it
+
 common_day!(day_01, Day01, u64, u64);
 common_day!(day_02, Day02, u64, u64);
 common_day!(day_03, Day03, u64, u64);
@@ -31,7 +35,6 @@ common_day!(day_06, Day06, u64, u64);
 common_day!(day_07, Day07, u64, u64);
 common_day!(day_08, Day08, u64, u64);
 common_day!(day_09, Day09, u64, u64);
-common_day!(day_10, Day10, u64, u64);
 
 #[wasm_bindgen(start)]
 pub fn start() {
