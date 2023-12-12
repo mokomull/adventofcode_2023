e156767a0dd8e59eaf62d3fb58f77f60504aa0f0
Matt Mullins <mmullins@mmlx.us>
2023-12-11 21:34:58 -0800

    day 12: try to count the options for a given line

--- day_12/src/lib.rs
+++ day_12/src/lib.rs
@@ -1,5 +1,6 @@
 use prelude::*;
 
+#[derive(Clone, Debug, PartialEq)]
 enum Spring {
     Good,
     Damaged,
@@ -19,6 +20,72 @@ impl Spring {
                 x => panic!("unexpected spring {x:?}"),
             })
             .collect_vec()
+    }
+}
+
+fn could_possibly_fit(springs: &[Spring], counts: &[u64]) -> bool {
+    if springs.is_empty() && !counts.is_empty() {
+        return false;
+    }
+
+    if counts.is_empty() {
+        return springs.iter().all(|s| s != &Damaged);
+    }
+
+    let mut counting = false;
+    let mut count = 0;
+
+    for (i, spring) in springs.iter().enumerate() {
+        match spring {
+            Good if counting => {
+                // we've found the first sequence of damaged springs
+                if count != counts[0] {
+                    // we definitely have a mismatch!
+                    return false;
+                }
+
+                return could_possibly_fit(&springs[(i + 1)..], &counts[1..]);
+            }
+            Good => (),
+            Damaged => {
+                counting = true;
+                count += 1;
+            }
+            // it's not *in*consistent yet, so it could *possibly* fit.
+            Unknown => return true,
+        }
+    }
+
+    // we haven't ruled it out...
+    return true;
+}
+
+fn count_options(springs: &[Spring], counts: &[u64]) -> u64 {
+    log::debug!("count_options: {springs:?} with counts {counts:?}");
+
+    if !could_possibly_fit(springs, counts) {
+        log::debug!("ruled it out");
+        return 0;
+    }
+
+    let first_unknown = springs.iter().position(|s| s == &Unknown);
+
+    if let Some(i) = first_unknown {
+        let mut count = 0;
+
+        // try it with a Good spring
+        let mut next = springs.to_vec();
+        next[i] = Good;
+        count += count_options(&next, counts);
+
+        // and try it with a Bad spring
+        next[i] = Damaged;
+        count += count_options(&next, counts);
+
+        return count;
+    } else {
+        // we had no unknowns, and we didn't rule it out, so this is the exactly-one way to arrange
+        return 1;
     }
 }
 
@@ -47,5 +114,24 @@ impl Day for Solution {
 
     fn part2(&self) -> anyhow::Result<u64> {
         anyhow::bail!("unimplemented")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_counts() {
+        let _ = env_logger::try_init();
+
+        fn do_line(line: &str) -> u64 {
+            let solution = Solution::new(line);
+            let (springs, counts) = solution.0.into_iter().next().unwrap();
+            count_options(&springs, &counts)
+        }
+
+        assert_eq!(1, do_line("#.#.### 1,1,3"));
+        assert_eq!(1, do_line("???.### 1,1,3"));
     }
 }
