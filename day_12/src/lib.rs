a2aa0a1bb66bc8654a1aaae3ef8674e006872760
Matt Mullins <mmullins@mmlx.us>
2023-12-11 21:11:53 -0800

    day 12: parser

--- day_12/src/lib.rs
+++ day_12/src/lib.rs
@@ -1,10 +1,44 @@
 use prelude::*;
 
-pub struct Solution();
+enum Spring {
+    Good,
+    Damaged,
+    Unknown,
+}
+
+use Spring::*;
+
+impl Spring {
+    fn parse(input: &str) -> Vec<Spring> {
+        input
+            .bytes()
+            .map(|b| match b {
+                b'.' => Good,
+                b'#' => Damaged,
+                b'?' => Unknown,
+                x => panic!("unexpected spring {x:?}"),
+            })
+            .collect_vec()
+    }
+}
+
+pub struct Solution(Vec<(Vec<Spring>, Vec<u64>)>);
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
-        Solution()
+        Solution(
+            input
+                .lines()
+                .map(|line| {
+                    let (springs, counts) = line.split_once(" ").expect("missing space");
+                    let counts = counts
+                        .split(',')
+                        .map(|x| x.parse().expect("not an integer"))
+                        .collect_vec();
+                    (Spring::parse(springs), counts)
+                })
+                .collect(),
+        )
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
