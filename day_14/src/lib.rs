6046a7c0036e4ef9ae0bc906ddda6d847a9fb1fd
Matt Mullins <mmullins@mmlx.us>
2023-12-15 18:50:05 -0800

    day 14: parse the map

--- day_14/src/lib.rs
+++ day_14/src/lib.rs
@@ -1,10 +1,39 @@
+use std::cmp::max;
+
 use prelude::*;
 
-pub struct Solution();
+pub struct Solution {
+    round: HashSet<(usize, usize)>,
+    cube: HashSet<(usize, usize)>,
+    max_x: usize,
+    max_y: usize,
+}
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
-        Solution()
+        let mut round = HashSet::new();
+        let mut cube = HashSet::new();
+        let mut max_x = 0;
+        let mut max_y = 0;
+
+        for (x, line) in input.lines().enumerate() {
+            for (y, c) in line.bytes().enumerate() {
+                if c == b'O' {
+                    round.insert((x, y));
+                } else if c == b'#' {
+                    cube.insert((x, y));
+                }
+                max_y = max(max_y, y);
+            }
+            max_x = max(max_x, x);
+        }
+
+        Solution {
+            round,
+            cube,
+            max_x,
+            max_y,
+        }
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
