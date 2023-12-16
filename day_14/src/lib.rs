d648ecf39885e796a438111999bea283a420bf95
Matt Mullins <mmullins@mmlx.us>
2023-12-15 22:03:30 -0800

    day 14: roll the platform around until a cycle
    This actually returned the correct result for the example on the first
    try, which really shocks me.

--- day_14/src/lib.rs
+++ day_14/src/lib.rs
@@ -1,18 +1,19 @@
-use std::cmp::max;
+use std::{cmp::max, collections::BTreeSet};
 
 use prelude::*;
 
+#[derive(Clone, Eq, Hash, PartialEq)]
 pub struct Solution {
-    round: HashSet<(usize, usize)>,
-    cube: HashSet<(usize, usize)>,
+    round: BTreeSet<(usize, usize)>,
+    cube: BTreeSet<(usize, usize)>,
     max_x: usize,
     max_y: usize,
 }
 
 fn upify(
-    round: &HashSet<(usize, usize)>,
-    cube: &HashSet<(usize, usize)>,
-) -> HashSet<(usize, usize)> {
+    round: &BTreeSet<(usize, usize)>,
+    cube: &BTreeSet<(usize, usize)>,
+) -> BTreeSet<(usize, usize)> {
     let mut new_round = round.clone();
     'outer: loop {
         for &(x, y) in new_round.iter() {
@@ -45,8 +46,8 @@ impl Solution {
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
-        let mut round = HashSet::new();
-        let mut cube = HashSet::new();
+        let mut round = BTreeSet::new();
+        let mut cube = BTreeSet::new();
         let mut max_x = 0;
         let mut max_y = 0;
 
@@ -84,6 +85,39 @@ impl Day for Solution {
     }
 
     fn part2(&self) -> anyhow::Result<u64> {
-        anyhow::bail!("unimplemented")
+        let mut seen = HashMap::new();
+        let mut state = self.clone();
+
+        for i in 0u64..1000000000 {
+            if let Some(last) = seen.get(&state) {
+                // find the state that matches the cycle
+                let cycle_length = i - last;
+                let target = last + ((1000000000 - last) % cycle_length);
+                state = seen
+                    .into_iter()
+                    .find(|(_state, iteration)| *iteration == target)
+                    .expect("somehow we went past an iteration but didn't find it again")
+                    .0;
+
+                break;
+            }
+            seen.insert(state.clone(), i);
+
+            // and roll the thing around all four directions
+            for _ in 0..4 {
+                state.round = upify(&state.round, &state.cube);
+                state = state.rotate_clockwise();
+            }
+        }
+
+        Ok(state
+            .round
+            .into_iter()
+            .map(|(x, _y)| {
+                let this = self.max_x as u64 - x as u64 + 1;
+                log::debug!("round at {:?} contributed {}", (x, _y), this);
+                this
+            })
+            .sum())
     }
 }
