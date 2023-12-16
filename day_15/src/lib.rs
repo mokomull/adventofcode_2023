bd483d175698891f0eb045ae477174599fd90d1b
Matt Mullins <mmullins@mmlx.us>
2023-12-15 17:26:45 -0800

    day 15: implement hash function

--- day_15/src/lib.rs
+++ day_15/src/lib.rs
@@ -1,5 +1,16 @@
 use prelude::*;
 
+fn hash(data: &[u8]) -> u8 {
+    let mut value = 0u8;
+
+    for &i in data.iter() {
+        value = value.wrapping_add(i);
+        value = value.wrapping_mul(17);
+    }
+
+    value
+}
+
 pub struct Solution();
 
 impl Day for Solution {
@@ -13,5 +24,16 @@ impl Day for Solution {
 
     fn part2(&self) -> anyhow::Result<u64> {
         anyhow::bail!("unimplemented")
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_hash() {
+        assert_eq!(30, hash(b"rn=1"));
+        assert_eq!(253, hash(b"cm-"));
     }
 }
