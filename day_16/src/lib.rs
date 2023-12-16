3ed418301abd990f5ddee58ee6472172e8410e02
Matt Mullins <mmullins@mmlx.us>
2023-12-15 21:06:00 -0800

    day 16: parser

--- day_16/src/lib.rs
+++ day_16/src/lib.rs
@@ -1,10 +1,45 @@
 use prelude::*;
 
-pub struct Solution();
+enum Tile {
+    Empty,
+    NS,
+    EW,
+    NeSw,
+    NwSe,
+}
+
+use Tile::*;
+
+enum Direction {
+    Up,
+    Left,
+    Down,
+    Right,
+}
+
+use Direction::*;
+
+pub struct Solution(Vec<Vec<Tile>>);
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
-        Solution()
+        Solution(
+            input
+                .lines()
+                .map(|line| {
+                    line.bytes()
+                        .map(|c| match c {
+                            b'.' => Empty,
+                            b'|' => NS,
+                            b'-' => EW,
+                            b'\\' => NwSe,
+                            b'/' => NeSw,
+                            x => panic!("unexpected character {x:?}")
+                        })
+                        .collect_vec()
+                })
+                .collect_vec(),
+        )
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
