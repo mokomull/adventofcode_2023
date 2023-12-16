9adb1ec2a0591c3f09f776e0b40d3bd0d5eb5be7
Matt Mullins <mmullins@mmlx.us>
2023-12-15 21:38:48 -0800

    day 16: factor-out the ray-tracing function

--- day_16/src/lib.rs
+++ day_16/src/lib.rs
@@ -42,30 +42,10 @@ impl Direction {
 
 pub struct Solution(Vec<Vec<Tile>>);
 
-impl Day for Solution {
-    fn new(input: &str) -> Self {
-        Solution(
-            input
-                .lines()
-                .map(|line| {
-                    line.bytes()
-                        .map(|c| match c {
-                            b'.' => Empty,
-                            b'|' => NS,
-                            b'-' => EW,
-                            b'\\' => NwSe,
-                            b'/' => NeSw,
-                            x => panic!("unexpected character {x:?}"),
-                        })
-                        .collect_vec()
-                })
-                .collect_vec(),
-        )
-    }
-
-    fn part1(&self) -> anyhow::Result<u64> {
+impl Solution {
+    fn count_from(&self, d: Direction, pos: (usize, usize)) -> u64 {
         let mut seen = HashSet::new();
-        let mut to_visit = VecDeque::from([(Right, (0, 0))]);
+        let mut to_visit = VecDeque::from([(d, pos)]);
 
         while let Some((d, (x, y))) = to_visit.pop_front() {
             if !seen.insert((d, (x, y))) {
@@ -120,11 +100,36 @@ impl Day for Solution {
             };
         }
 
-        Ok(seen
-            .into_iter()
+        seen.into_iter()
             .map(|(_dir, pos)| pos)
             .collect::<HashSet<_>>()
-            .len() as u64)
+            .len() as u64
+    }
+}
+
+impl Day for Solution {
+    fn new(input: &str) -> Self {
+        Solution(
+            input
+                .lines()
+                .map(|line| {
+                    line.bytes()
+                        .map(|c| match c {
+                            b'.' => Empty,
+                            b'|' => NS,
+                            b'-' => EW,
+                            b'\\' => NwSe,
+                            b'/' => NeSw,
+                            x => panic!("unexpected character {x:?}"),
+                        })
+                        .collect_vec()
+                })
+                .collect_vec(),
+        )
+    }
+
+    fn part1(&self) -> anyhow::Result<u64> {
+        Ok(self.count_from(Right, (0, 0)))
     }
 
     fn part2(&self) -> anyhow::Result<u64> {
