b67ef08e8b6ae5e4082ef758b781a538f99a16a2
Matt Mullins <mmullins@mmlx.us>
2023-12-16 21:02:58 -0800

    day 17: template

--- omnibus/src/lib.rs
+++ omnibus/src/lib.rs
@@ -40,6 +40,7 @@ common_day!(day_12, Day12, u64, u64);
 common_day!(day_14, Day14, u64, u64);
 common_day!(day_15, Day15, u64, u64);
 common_day!(day_16, Day16, u64, u64);
+common_day!(day_17, Day17, u64, u64);
 
 #[wasm_bindgen(start)]
 pub fn start() {
