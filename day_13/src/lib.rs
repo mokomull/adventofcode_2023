95c3ec9bd4eb6c34a6ab7baaa6c773d1f1b90813
Matt Mullins <mmullins@mmlx.us>
2023-12-17 18:01:15 -0800

    day 13: template

---
+++ day_13/src/lib.rs
@@ -0,0 +1,17 @@
+use prelude::*;
+
+pub struct Solution();
+
+impl Day for Solution {
+    fn new(input: &str) -> Self {
+        Solution()
+    }
+
+    fn part1(&self) -> anyhow::Result<u64> {
+        anyhow::bail!("unimplemented");
+    }
+
+    fn part2(&self) -> anyhow::Result<u64> {
+        anyhow::bail!("unimplemented");
+    }
+}
